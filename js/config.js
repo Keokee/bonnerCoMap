641ad23e5648f023564d57929b90e57904855433
Ethan <spaminol@gmail.com>
2021-12-17 11:22:54 -0800

    initial

---
+++ js/config.js
@@ -0,0 +1,18 @@
+initBounds = [-109.609,45.981,-112.841,44.936]
+
+mapLocations = {
+  'init': {
+    center: [-110.528,44.77],
+    pitch: 0,
+    bearing: 0,
+    zoom: 7.5,
+    speed: 0.4
+  },
+  'madGalSwPer': {
+    center: [-111.2175, 45.353],
+    pitch: 14,
+    bearing: 0,
+    zoom: 9.5,
+    speed: 0.4
+  },
+}
